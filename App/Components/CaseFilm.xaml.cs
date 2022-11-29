642f14f9c1cc4a449ad4c461d5c806ed7ce57044
GentilBastien <bastien.ga@free.fr>
2022-11-29 23:06:43 +0100

    WPF menu accueil

---
+++ App/Components/CaseFilm.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MaVideotheque.Components
+{
+    public partial class CaseFilm : UserControl
+    {
+        public string Nom_film { get; set; }
+        public string Real_film { get; set; }
+        public string Annee_film {get; set; }
+        public string Genres_film { set; get; }
+        public string SrcImg { set; get; }
+
+        public CaseFilm()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+    }
+}
