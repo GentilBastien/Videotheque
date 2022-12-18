da2955af8c52ff8ca4ea3d50708ca540cda71429
GentilBastien <bastien.ga@free.fr>
2022-12-18 11:40:52 +0100

    pull

--- App/MainWindow.xaml.cs
+++ App/MainWindow.xaml.cs
@@ -18,8 +18,8 @@ namespace MaVideotheque
 {
     public partial class MainWindow : Window
     {
-        //public static string CONNECTION_STRING = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Users\basti\Desktop\MIASHS\M2\S9\MaVideotheque\App\Database.mdf;Integrated Security=True;Connect Timeout=30";
-        public static string CONNECTION_STRING = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\fbelh\source\repos\Videotheque\App\Database.mdf;Integrated Security=True;Connect Timeout=30";
+        public static string CONNECTION_STRING = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Users\basti\Desktop\MIASHS\M2\S9\MaVideotheque\App\Database.mdf;Integrated Security=True;Connect Timeout=30";
+        //public static string CONNECTION_STRING = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\fbelh\source\repos\Videotheque\App\Database.mdf;Integrated Security=True;Connect Timeout=30";
 
 
 
